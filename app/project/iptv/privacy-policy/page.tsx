36631fbc568f3ca68cd3b24751d5aa8d91933276
Sandri Kun <sndriirsyd.kun@gmail.com>
2024-12-14 16:11:54 +0700

    m

---
+++ app/project/iptv/privacy-policy/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const PrivacyPolicy = () => {
+  return (
+    <>
+     <div>
+      <h1>Project Config</h1>
+    </div>
+    </>
+  );
+};
+
+export default PrivacyPolicy;
